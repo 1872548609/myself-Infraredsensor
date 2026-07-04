0a3f34b88b729169f8abcdcd007bcbaf36b51406
baocheng <1872548609@qq.com>
2026-07-04 12:07:47 +0800

    修改文件路径

---
+++ UN800Y红外对射/2.程序/新版本测试版本/接收/接收-第十四版20260617-测试版本/发射/发射/Example/gpio/demo/source/main.c
@@ -0,0 +1,57 @@
+/***********************************************************************
+ * Copyright (c)  2017 - 2021, Unicmicro Co.,Ltd .
+ * All rights reserved.
+ * Filename    : main.c
+ * Description : main source file
+ * Author(s)   : Will
+ * version     : V1.0
+ * Modify date : 2021-04-27
+ ***********************************************************************/     
+#include "system_um800y.h"
+#include "app.h"
+#include "gtimer.h"
+#include "pwm.h"
+#include "common.h"
+#include "config.h"
+#include "gpio.h"
+#include "adc.h"
+
+void GPIO_Init(void);
+void ADC_Init(void);
+void gpio_int_pro(void);
+void gtimer0_UECallBack(void);
+
+void main(void)  
+{  		
+	system_init(); // 24Mhz
+	GPIO_Init();
+	uart_init();
+	
+	pwm2_init(24000,600,LOW);
+	pwm2_start();
+    
+	while(1) 
+	{
+
+	}
+}
+
+
+void GPIO_Init(void)
+{
+	//红色指示灯
+	gpio_init(P1_3);
+	gpio_dir_set(P1_3, GPIO_DIR_OUT);
+	gpio_dr_set(P1_3, GPIO_SR_HIGH);
+	gpio_io_set(P1_3, GPIO_HIGH);
+	
+	//脉冲发射口
+	REG_P14_CFG=0x20;
+	gpio_sr_set(P1_4,GPIO_SR_HIGH);
+	
+	//脉冲接收口
+	REG_P15_CFG=0x00;
+	gpio_init(P1_5);                          //时钟初始化，模块正常工作
+	gpio_dir_set(P1_5, GPIO_DIR_IN);          //配置为输入
+	gpio_in_enable(P1_5, IN_ENABLE);          //输入使能 
+}
